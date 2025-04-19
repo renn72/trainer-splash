daa66cd4acec91cc2ba9bf53fbc5b1be8f0224f6
renn <renn@warner.systems>
2025-04-19 14:02:25 +1000

    extend

--- src/app/_components/email-input.tsx
+++ src/app/_components/email-input.tsx
@@ -18,50 +18,52 @@ export default function EmailInput() {
     e.preventDefault()
     console.log({ email, terms })
     if (terms) return
-    createEmail.mutate({ email, })
+    createEmail.mutate({ email })
   }
 
   return (
-    <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-99'>
-    <div className='relative'>
-      <BarbellIcon
+    <div className='absolute top-1/2 left-1/2 z-99 -translate-x-1/2 -translate-y-1/2'>
+      <div className='relative'>
+        <BarbellIcon
           size={512}
           color='#00000080'
-          className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10 rotate-135 opactiy-25'
+          className='opactiy-25 absolute top-1/2 left-1/2 -z-10 -translate-x-1/2 -translate-y-1/2 rotate-135'
         />
-      <h1 className='text-3xl lg:text-4xl font-bold text-white uppercase w-full text-center mb-4'>it's coming</h1>
-      <form
-        onSubmit={handleSubmit}
-        className='flex gap-4'
-      >
-        <div style={{ position: 'absolute', left: '-9999px' }}>
-          <label htmlFor='acceptTerms'>Check this</label>
-          <input
-            type='checkbox'
-            name='acceptTerms'
-            tabIndex={-1}
-            onChange={(e) => setTerms(e.target.checked)}
-            id='acceptTerms'
-          />
-        </div>
-          <div className='flex flex-col lg:flex-row gap-2 lg:-gap-4'>
-          <input
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className='w-full z-100 font-semibold text-base lg:text-lg rounded-full bg-black/20 px-4 py-2 m-[1px] text-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white/80 focus-visible:bg-black/90'
-          />
-        <button
-          type='submit'
-          className='rounded-full z-100 bg-white/70 px-6 py-1 font-semibold transition hover:bg-white/20 text-sm lg:text-base'
-          disabled={createEmail.isPending}
+        <h1 className='mb-4 w-full text-center text-3xl font-bold text-white uppercase lg:text-4xl'>
+          it's coming
+        </h1>
+        <form
+          onSubmit={handleSubmit}
+          className='flex gap-4'
         >
-          {createEmail.isPending ? 'Submitting...' : 'Submit'}
-        </button>
-    </div>
-      </form>
-    </div>
+          <div style={{ position: 'absolute', left: '-9999px' }}>
+            <label htmlFor='acceptTerms'>Check this</label>
+            <input
+              type='checkbox'
+              name='acceptTerms'
+              tabIndex={-1}
+              onChange={(e) => setTerms(e.target.checked)}
+              id='acceptTerms'
+            />
+          </div>
+          <div className='lg:-gap-4 flex flex-col gap-2 lg:flex-row'>
+            <input
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className='z-100 m-[1px] min-w-[230px] w-full rounded-full bg-black/20 px-4 py-2 text-base font-semibold text-white focus-visible:bg-black/90 focus-visible:ring-2 focus-visible:ring-white/80 focus-visible:outline-none lg:text-lg'
+            />
+            <button
+              type='submit'
+              className='z-100 rounded-full bg-white/70 px-6 py-1 text-sm font-semibold transition hover:bg-white/20 lg:text-base'
+              disabled={createEmail.isPending}
+            >
+              {createEmail.isPending ? 'Submitting...' : 'Submit'}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
